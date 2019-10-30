af975488bc7ed5c88f20ccb28c0c3120f00e0327
Marina Alonso Poal <43316350+marinalpo@users.noreply.github.com>
2019-10-30 18:51:34 -0400

    Add files via upload

--- Experiments2_full.py
+++ Experiments2_full.py
@@ -1,14 +1,19 @@
 # Experiments2_full.m -----------------------------------------------------------------
 # Exp 1: Fix Ns and D, run for different Np and record the running time and convergence
+import numpy as np
+import cvxpy as cp
+from time import time
+from utils import *
+
 np.random.seed(2020)
 
-Ns = 3
-D = 3
-Npoints = Ns*(np.arange(20)+1)
+Ns = 3  # Number of subspaces
+D = 3  # Number of dimensions
+Npoints = Ns*(np.arange(20)+1)  # Number of points per dimension
 N_tests = 8
-RwHopt = RwHoptCond(20, 0.97, 0)
+RwHopt = RwHoptCond(20, 0.97, 0)  # Conditions for reweighted heuristics
 delta = 0.1
-eps = 0.1
+eps = 0.1  # Noise bound
 
 # Data dumps
 iterations = np.zeros([len(Npoints), N_tests])
@@ -26,8 +31,8 @@ Xp, ss_ind = generatePoints(normals, num_points[0].astype(int), eps, 5)
 
 # Rank 1 on moment matrix
 RwHopt.corner = 0
-
-# FUNCTION begins ------------------------------------- SSC_Yalmip_Full(Xp, eps, Ns, RwHopt, delta)
+# R, S, runtime, rank1ness = SSC_CVXPY_Full(Xp, eps, Ns, RwHopt, delta)
+# FUNCTION begins ---------------------------------------------------------------------------------------
 
 # Define dimension and number of points
 [D, S] = Xp.shape
@@ -35,8 +40,8 @@ Nm = 1 + Ns*(Np + D)  # Size of the overall matrix
 Nc = 1 + Ns*D  # Size of the top left corner
 
 # Define index entries
-ind_r = lambda i, d: (1+(i-1)*D+d)
-ind_s = lambda i, j: (1+Ns*D+(j-1)*Ns+i)
+ind_r = lambda i, d: (1+i*D+d)
+ind_s = lambda i, j: (1+Ns*D+(j-1)*Ns+i)  # REPASSAR
 tic = time()
 
 # Create variables
@@ -44,39 +49,60 @@ X = cp.Variable((Nm, Nm), PSD=True)  # X is PSD
 C = []  # Fixed constraints
 Citer = []  # Constraints that will change through iterations
 for i in range(Ns):  # For each subspace
-  ind = ind_r(i+1,(np.arange(D)+1))
-  C = C + [cp.trace(X[np.ix_(ind,ind)]) == 1]
-  for j in range(Np):  # For each subpoint
-    C = C + [ X[1,ind_s(i,j)] == X[ind_s(i,j),ind_s(i,j)] ]  # s_ij and (s_ij)^2 are equal
-    C = C + [ X[ind,ind_s(i,j)].T*Xp[:,j] - eps*X[1,ind_s(i,j)] <= 0]  # First inequality of abs. value
-    C = C + [ - X[ind,ind_s(i,j)].T*Xp[:,j] - eps*X[1,ind_s(i,j)] <= 0] # Second inequality of abs. value
-    C = C + [cp.sum(X[np.ix_(ind_r(np.arange(Ns),j))]) == 1]  # Sum of labels for each point equals 1
+    ind = ind_r(i,(np.arange(D)))
+    C = C + [cp.trace(X[np.ix_(ind,ind)]) == 1]
+    for j in range(Np):  # For each subpoint
+        C = C + [ X[1,ind_s(i,j)] == X[ind_s(i,j),ind_s(i,j)] ]  # s_ij and (s_ij)^2 are equal
+        C = C + [ X[ind,ind_s(i,j)].T*Xp[:,j] - eps*X[1,ind_s(i,j)] <= 0]  # First inequality of abs. value
+        C = C + [ - X[ind,ind_s(i,j)].T*Xp[:,j] - eps*X[1,ind_s(i,j)] <= 0] # Second inequality of abs. value
+        C = C + [cp.sum(X[np.ix_(ind_r(np.arange(Ns),j))]) == 1]  # Sum of labels for each point equals 1
+
 C = C + [X[0,0]==1]  # 1 on the upper left corner
 
-##### TRADUIR options = sdpsettings('solver','mosek', 'verbose', 0, 'cachesolvers', 1);
-
 if RwHopt.corner:
-  # W = np.eye(Nc) + delta*np.random.randn(Nc,Nc)
-  W = np.eye(Nc)
+    # W = np.eye(Nc) + delta*np.random.randn(Nc,Nc)
+    W = np.eye(Nc)
 else:
-  # W = np.eye(Nc) + delta*np.random.randn(Nm,Nm)
-  W = np.eye(Nm)
+    # W = np.eye(Nc) + delta*np.random.randn(Nm,Nm)
+    W = np.eye(Nm)
 
 # Solve the problem
 rank1ness = np.zeros([RwHopt.maxIter,1])
 bestX = X
 for iter in range(RwHopt.maxIter):
-  if RwHopt.corner:
-    M = X[1:Nc, 1:Nc]
-    tr = cp.trace(W.T*M)
-  else:
-    tr = cp.trace(W.T*X)
+    if RwHopt.corner:
+        M = X[1:Nc, 1:Nc]
+        obj = cp.Minimize(cp.trace(W.T*M))
+        prob = cp.Problem(obj, C)
+        sol = prob.solve(solver=cp.MOSEK, verbose=False)
+        [Si, Ui] = np.linalg.eig(M.value)
+    else:
+        obj = cp.Minimize(cp.trace(W.T*X))
+        prob = cp.Problem(obj, C)
+        sol = prob.solve(solver=cp.MOSEK, verbose=False)
+        #test = X[np.ix_(ind_r(np.arange(Ns),j))]
+        #print('test:',test.value)
+        [Si, Ui] = np.linalg.eig(X.value)
 
-### MISSING
+    sorted_eigs = -np.sort(-Si)
+    rank1ness_curr = sorted_eigs[0]/np.sum(sorted_eigs)
+    rank1ness[iter]=rank1ness_curr
+    if np.max(rank1ness) == rank1ness[iter]:
+        bestX = X.value
+    if np.min(rank1ness[iter])>RwHopt.eigThres:
+        break
 
 runtime = time() - tic
+s = X[ind_r(Ns,D):,0]
+S = cp.reshape(s,(Ns,Np))
+print(S.value)
+R = ''
+for i in range(Ns):
+    a = 0
+
 
 # FUNCTION ends -------------------------------------
 
 # Rank 1 on corner
 RwHopt.corner = 1
+
