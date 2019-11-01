e230228d0b2af06d5efab7fce2a2eb07e688b13c
marinalpo <malonsopoal@gmail.com>
2019-11-01 11:03:20 -0400

    Created data_dump

--- Experiments2_full.py
+++ Experiments2_full.py
@@ -1,6 +1,7 @@
 # Experiments2_full.m -----------------------------------------------------------------
 # Exp 1: Fix Ns and D, run for different Np and record the running time and convergence
 import numpy as np
+import matplotlib.pyplot as plt
 from tempfile import TemporaryFile
 from utils import *
 from SSC_CVXPY_Full import *
@@ -10,10 +11,12 @@ np.random.seed(2020)
 Ns = 3  # Number of subspaces
 D = 3  # Number of dimensions
 Npoints = Ns*(np.arange(20)+1)  # Number of points per dimension
+Npoints = Ns*(np.arange(2)+1)  # Number of points per dimension
 N_tests = 8
+N_tests = 1
 RwHopt = RwHoptCond(20, 0.97, 0)  # Conditions for reweighted heuristics
 delta = 0.1
-eps = 0.2  # Noise bound
+eps = 0.1  # Noise bound
 
 # Data dumps
 Iterations = np.zeros([len(Npoints), N_tests])
@@ -31,11 +34,17 @@ for k in range(len(Npoints)):
         # Rank 1 on moment matrix
         RwHopt.corner = 0
         R, S, runtime, rank1ness = SSC_CVXPY_Full(Xp, eps, Ns, RwHopt, delta)
+        print('\n S:', np.around(S.value, decimals=3))
         # test = cp.sum(S, axis = 0)
         # print('test:', test.value)
         Iterations[k,t] = len(rank1ness)
         Runtime[k,t] = runtime
         Rank1ness.append(rank1ness)
 
-np.savez('experiments_2_dump_full.npz', name1 = Iterations, name2 = Runtime, name3 = Rank1ness, name4 = Ns, name5 = D, name6 = Npoints)
+np.savez('/Users/marinaalonsopoal/PycharmProjects/SubspaceClustering/data_dump/experiments_2_dump_full.npz', name1 = Iterations, name2 = Runtime, name3 = Rank1ness, name4 = Ns, name5 = D, name6 = Npoints)
 
+plt.plot(Npoints,np.mean(Runtime,axis=1))
+plt.title('Runtime vs. Number of points')
+plt.xlabel('Np')
+plt.ylabel('time (s)')
+plt.show()
